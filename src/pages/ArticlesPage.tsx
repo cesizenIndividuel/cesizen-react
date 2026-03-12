2290838e675bb5fd87bf4a2824d89bb7568a154c
LERMECHAIN <laura.ledoux@viacesi.fr>
2026-03-12 13:52:40 +0100

    feat : mise en place layout

---
+++ src/pages/ArticlesPage.tsx
@@ -0,0 +1,8 @@
+export function ArticlesPage() {
+  return (
+    <div>
+      <h1>Gestion des articles</h1>
+      <p>Créer, modifier et publier les articles.</p>
+    </div>
+  );
+}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import { AdminLayout } from "../layouts/AdminLayout";
 import { LoginPage } from "../pages/LoginPage";
 import { DashboardPage } from "../pages/DashboardPage";
+import { UsersPage } from "../pages/UsersPage";
+import { ArticlesPage } from "../pages/ArticlesPage";
+import { CategoriesPage } from "../pages/CategoriesPage";
 
 export const router = createBrowserRouter([
   {
@@ -9,6 +13,24 @@ export const router = createBrowserRouter([
   },
   {
     path: "/admin",
-    element: <DashboardPage />,
+    element: <AdminLayout />,
+    children: [
+      {
+        index: true,
+        element: <DashboardPage />,
+      },
+      {
+        path: "users",
+        element: <UsersPage />,
+      },
+      {
+        path: "articles",
+        element: <ArticlesPage />,
+      },
+      {
+        path: "categories",
+        element: <CategoriesPage />,
+      },
+    ],
   },
 ]);
