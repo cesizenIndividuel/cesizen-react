c64c71b6d39397306448d90699a9874e317261e1
LERMECHAIN <laura.ledoux@viacesi.fr>
2026-03-12 11:34:39 +0100

    feat : mise en place des pages login et dashbord

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,8 @@
+export function DashboardPage() {
+  return (
+    <div>
+      <h1>Dashboard admin</h1>
+      <p>Bienvenue dans le back-office CESIZen</p>
+    </div>
+  );
+}
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,8 @@
+export function LoginPage() {
+  return (
+    <div>
+      <h1>Connexion admin</h1>
+      <p>Page de connexion CESIZen</p>
+    </div>
+  );
+}
