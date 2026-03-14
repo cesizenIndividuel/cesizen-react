7f4b55c03dc65126567abcda32cbe7cbe07db225
LERMECHAIN <laura.ledoux@viacesi.fr>
2026-03-14 22:41:37 +0100

    refactor : clen code

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,36 +1,36 @@
 import { apiClient } from "./client";
 
-//Ce que le front envoie au back lors que le user se connecte
+// Ce que le front envoie au back lors que le user se connecte
 export type LoginPayload = {
   email: string;
   password: string;
 };
 
-//type de user identifié
+// Type de user identifié
 export type AuthUser = {
   id: string;
   role: "USER" | "ADMIN";
   isActive: boolean;
 };
 
-//Ce que le back va envoyer apres un login reussi
+// Ce que le back va envoyer après un login réussi
 export type LoginResponse = {
   accessToken: string;
   user: AuthUser;
 };
 
-//reponse du back quand demande de nouveau token 
+// Réponse du back quand demande de nouveau token
 export type RefreshResponse = {
   accessToken: string;
 };
 
-//appel de POST http://localhost:3000/api/auth/login
+// Appel de POST /auth/login
 export async function login(payload: LoginPayload) {
   const response = await apiClient.post<LoginResponse>("/auth/login", payload);
   return response.data;
 }
 
-//Demande au back un nouvel access token 
+// Demande au back un nouvel access token
 export async function refreshToken() {
   const response = await apiClient.post<RefreshResponse>("/auth/refresh");
   return response.data;
