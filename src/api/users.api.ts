d4ed7e88c7c8d933da96bb1ad1210f7be0f04c63
LERMECHAIN <laura.ledoux@viacesi.fr>
2026-03-14 15:58:48 +0100

    feat : creation de user

--- src/api/users.api.ts
+++ src/api/users.api.ts
@@ -1,18 +1,32 @@
 import { apiClient } from "./client";
 import type { User } from "../types/user";
 
-//Récupère la liste des utilisateurs depuis l’API.
+type CreateUserPayload = {
+  firstName: string;
+  lastName: string;
+  pseudo: string;
+  email: string;
+  password: string;
+  role: "USER" | "ADMIN";
+};
+
+// Récupère la liste des utilisateurs depuis l’API.
 export async function getUsers(): Promise<User[]> {
   const response = await apiClient.get("/users");
   return response.data;
 }
 
-//Mise a jour d'un user
+// Mise à jour du statut d'un utilisateur
 export async function updateUserStatus(userId: string, isActive: boolean) {
-  const response = await apiClient.patch<User>(
-    `/users/${userId}/status`,
-    { isActive }
-  );
+  const response = await apiClient.patch<User>(`/users/${userId}/status`, {
+    isActive,
+  });
 
+  return response.data;
+}
+
+// Création d'un utilisateur
+export async function createUser(payload: CreateUserPayload): Promise<User> {
+  const response = await apiClient.post("/users", payload);
   return response.data;
 }
